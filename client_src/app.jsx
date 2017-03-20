50f951d986eb322a8b0a3ecf84c3a565105f6b67
harapan1231 <senang.harapan@gmail.com>
2017-03-20 23:35:04 +0900

    Update page using inferno.js

--- client_src/app.jsx
+++ client_src/app.jsx
@@ -1,24 +1,69 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
-class MyComponent extends Component {
+class SubjectDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.subjects = ["k", "l", "m"];
+  }
+  render() {
+    return (
+      <select className="column">{
+        this.subjects.map((subject, i) => {
+          return (
+            <option>{ subject }</option>
+          )
+        })
+      }</select>
+    )
+  }
+}
+
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0
-    }
+    };
+    this.rows = [
+      {name: "sale",  subject: "k", io: "i", amount: 10000},
+      {name: "food",  subject: "l", io: "o", amount: 1000},
+      {name: "paper", subject: "m", io: "o", amount: 2000},
+      {name: "train", subject: "",  io: "o", amount: 1000},
+      {name: "hotel", subject: "k", io: "o", amount: 5000},
+    ];
+    this.inputOnChange = function(e) {
+      // TODO
+      console.log(e);
+    };
+    this.sum = 0;
   }
   render() {
     return (
       <div>
-        <h1>Header!</h1>
-        <span>Counter is at: { this.state.counter }</span>
+        <h1>Nasca</h1>
+        <div className="container">
+          <span>Counter is at: { this.state.counter }</span>
+          {
+            this.rows.map((row, i) => {
+              return (
+                <div className="row">
+                  <span className="column">{ row.name }</span>
+                  <SubjectDropdown />
+                  <span className="column">{ row.io === "i" ? "Income" : "Outcome" }</span>
+                  <input type="text" onChange={ this.inputOnChange } value={ row.amount } />
+                </div>
+              )
+            })
+          }
+          <div className="row">{ this.rows.reduce((x, y) => { let z = x; z.amount += y.amount; return z }).amount }</div>
+        </div>
       </div>
     )
   }
 }
 
 Inferno.render(
-  <MyComponent />,
+  <App />,
   document.getElementById("app")
 );
