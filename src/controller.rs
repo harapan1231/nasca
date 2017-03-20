59c5658fde2e3843f6006bf0752fc71e05001e43
harapan1231 <senang.harapan@gmail.com>
2017-03-21 00:17:03 +0900

    Enable to serve binary files

--- src/controller.rs
+++ src/controller.rs
@@ -1,14 +1,15 @@
 extern crate hyper;
 extern crate hyper_router;
 
-use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 
 use hyper::server::{Request, Response};
 use hyper::uri::RequestUri::AbsolutePath;
 
 pub fn root(_: Request, res: Response) {
-  serve("www/index.html", res);
+  serve("/www/index.html", res);
 }
 
 pub fn serve_static(req: Request, res: Response) {
@@ -17,6 +18,7 @@ pub fn serve_static(req: Request, res: Response) {
       serve(path, res);
     }
     _ => {
+      println!("{:#?}", req.uri);
       panic!();
     }
   };
@@ -29,8 +31,9 @@ pub fn greet(req: Request, res: Response) {
 
 fn serve(path: &str, res: Response) {
   let p = if path.starts_with("/") { [".", path].concat() } else { path.to_string() };
-  let mut file = File::open(p).unwrap();
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).unwrap();
-  res.send(contents.as_bytes()).unwrap();
+  let file = File::open(p).unwrap();
+  let mut buf_reader = BufReader::new(file);
+  let mut contents = Vec::new();
+  buf_reader.read_to_end(&mut contents).unwrap();
+  res.send(&contents).unwrap();
 }
