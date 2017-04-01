e0040b50d9615af13a3a7c574ad9c37383d0254d
harapan1231 <senang.harapan@gmail.com>
2017-04-01 20:10:20 +0900

    Divide client components

--- client_src/app.jsx
+++ client_src/app.jsx
@@ -1,23 +1,7 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
-class SubjectDropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.subjects = ["k", "l", "m"];
-  }
-  render() {
-    return (
-      <select className="column">{
-        this.subjects.map((subject, i) => {
-          return (
-            <option>{ subject }</option>
-          )
-        })
-      }</select>
-    )
-  }
-}
+import { SubjectDropdown } from './components.jsx'
 
 class App extends Component {
   constructor(props) {
